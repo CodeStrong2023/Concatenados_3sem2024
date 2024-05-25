15f96e26680dab704b7a258fdd22cd7bf4883d0a
Franco Pieroni <71734317+franco594@users.noreply.github.com>
2024-05-24 22:02:34 -0300

    Java Clase 5
    Conversión de objetos, Downcasting, Upcasting, hashcode, equals, abstracción

---
+++ Java/Lección05/ConversionObjetos/src/domain/TipoEscritura.java
@@ -0,0 +1,22 @@
+package domain;
+
+public enum TipoEscritura {
+    
+    CLASICO ("Escritura a mano"),
+    MODERNO ("Escritura digital");
+    
+    private final String descripcion;
+    
+    private TipoEscritura (String descripcion){ // Constructor
+        this.descripcion = descripcion;
+        
+    }
+    
+    // Método get
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+    
+    
+}
