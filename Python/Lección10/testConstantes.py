b34e98cfa79eca892170dcb63ad2b9e28d49af3e
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2024-06-05 21:38:41 -0300

    Revert "Merge branch 'Gastón' of https://github.com/CodeStrong2023/Concatenados_3sem2024 into Gastón"
    This reverts commit 85aeeb7970a6c8c6fbb18eebcf5d3b668d6e9a6b, reversing
    changes made to 85b9e05f35ade11fc4c43217f3ae9fb1dd697fe3.

--- Python/Lección10/testConstantes.py
+++
@@ -1,8 +0,0 @@
-from Constantes import *
-from Constantes import Matematicas as Mate # Asignamos un alias a Matematica para que sea mas fácil de llamar
-
-print(MI_CONSTANTE)
-# MI_CONSTANTE = 'Aquí esta el nuevo valor de esta constante'
-# print(MI_CONSTANTE)
-
-print(Mate)
