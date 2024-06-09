786372414d7d6609c6a165b8f25470723bdb54e8
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2024-06-08 22:40:34 -0300

    Clase 8 Python

---
+++ Python/Leccion04/test_bd.sql
@@ -0,0 +1,13 @@
+--Consulta infromación seleccionada
+--SELECT * FROM persona WHERE id_persona IN (1,2,3)
+
+--Ingresamos un nuevo elemento a la tabla
+--INSERT INTO persona(nombre, apellido, email) VALUES ('Susana', 'Lara', 'slara@mail.com')
+
+--Hacemos la consulta para ver toda la información de la tabla
+--SELECT * FROM persona
+
+--UPDATE persona SET nombre = 'Ivonne', apellido = 'Esparza', email = 'iesparza@mail.com' WHERE id_persona = 3
+SELECT * FROM persona
+
+--DELETE FROM persona WHERE id_persona = 3
